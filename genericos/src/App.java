07a16ec45dca9221de21866fd24e4f699132e615
Martin <mgarcas320@g.educaand.es>
2025-01-15 12:33:07 +0100

    stream

--- genericos/src/App.java
+++ genericos/src/App.java
@@ -1,6 +1,13 @@
 //import com.jve.modelo.Caja;
 //package com.jve;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Random;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import com.jve.dto.AlumnoDTO;
 import com.jve.dto.AlumnoToDTOConverter;
@@ -61,9 +68,9 @@ public class App {
         System.out.println("------------------------------------------");
         // Crear una lista de alumnos
         Lista<Alumno> listaDeAlumnos = new Lista<>();
-        listaDeAlumnos.agregar(new Alumno("Martín", "Calderón"));
-        listaDeAlumnos.agregar(new Alumno("Ana", "Gómez"));
-        listaDeAlumnos.agregar(new Alumno("Carlos", "Pérez"));
+        listaDeAlumnos.agregar(new Alumno("Martín", "Calderón", 27));
+        listaDeAlumnos.agregar(new Alumno("Ana", "Gómez", 24));
+        listaDeAlumnos.agregar(new Alumno("Carlos", "Pérez", 22));
 
         //Método 1
         // Crear el conversor de Alumno a AlumnoDTO
@@ -96,9 +103,9 @@ public class App {
 
         System.out.println("Tamaño de la pila antes de agregar alumnos: " + pilaDeAlumnos.size());
 
-        Alumno alumnoPila1 = new Alumno("Daniel", "García");
-        Alumno alumnoPila2 = new Alumno("Fernando", "Gómez");
-        Alumno alumnoPila3 = new Alumno("Luis", "Martínez");
+        Alumno alumnoPila1 = new Alumno("Daniel", "García", 32);
+        Alumno alumnoPila2 = new Alumno("Fernando", "Gómez", 20);
+        Alumno alumnoPila3 = new Alumno("Luis", "Martínez", 44);
         
         pilaDeAlumnos.push(alumnoPila1);
         pilaDeAlumnos.push(alumnoPila2);
@@ -130,5 +137,78 @@ public class App {
 
         // Comprobar si la pila está vacía después de limpiar
         System.out.println("¿Está la pila vacía después de limpiarla? " + pilaDeAlumnos.empty());
+
+        
+        System.out.println("------------------------------------------");
+
+        // Alumno, nombre y edad, equals, dos alumnos son iguales si se llaman igual y tienen la misma edad. 
+        // Alumno comparable natural order...además otro comparator por edad
+
+        // Listado por nombre y edad (orden natural)
+        List<Alumno> alumnos = new ArrayList<>();
+        
+        Alumno a1 = new Alumno("Juan", "Pérez", 16);
+        Alumno a2 = new Alumno("Ana", "García", 18);
+        Alumno a3 = new Alumno("Pepe", "Lopez", 22);
+        Alumno a4 = new Alumno("Ana", "Martín", 17);
+        Alumno a5 = new Alumno("Pepe", "Sánchez", 20);
+
+        alumnos.add(a1);
+        alumnos.add(a2);
+        alumnos.add(a3);
+        alumnos.add(a4);
+        alumnos.add(a5);
+        
+        System.out.println("Orden natural:");
+        alumnos.stream().sorted().forEach(System.out::println);
+        System.out.println("------------------------------------------");
+
+        //listado de los mayores de edad
+        System.out.println("Listado de los mayores de edad:");
+        alumnos.stream().filter(alumno -> alumno.getEdad() >= 18).forEach(System.out::println);
+        System.out.println("------------------------------------------");
+
+        //listado de los que empiezan por "a".
+        System.out.println("Listado de los que empiezan por 'a':");
+        alumnos.stream().filter(alumno -> alumno.getNombre().startsWith("A") || alumno.getNombre().startsWith("a")).forEach(System.out::println);
+        System.out.println("------------------------------------------");
+
+        // Listado ordenado por edad (ascendente)
+        System.out.println("Listado ordenado por edad (ascendente):");
+        alumnos.stream().sorted(Alumno.compararPorEdad).forEach(System.out::println);
+        System.out.println("------------------------------------------");
+
+        // Listado ordenado por edad inversa(descendente)
+        System.out.println("Listado ordenado por edad (descendente):");
+        alumnos.stream().sorted(Alumno.compararPorEdad.reversed()).forEach(System.out::println);
+        System.out.println("------------------------------------------");
+
+        // Cuantos alumnos son mayores de edad
+        System.out.println("¿Cuántos alumnos son mayores de edad?:");
+        System.out.println(alumnos.stream().filter(alumno -> alumno.getEdad() >= 18).count());
+        System.out.println("------------------------------------------");
+
+        // Hay algún alumno con 20 años?
+        System.out.println("¿Hay algún alumno con 20 años?:");
+        System.out.println(alumnos.stream().anyMatch(alumno -> alumno.getEdad() == 20));
+        System.out.println("------------------------------------------");
+
+        // Hay algún alumno mayor de edad?
+        System.out.println("¿Hay algún alumno mayor de edad?:");
+        System.out.println(alumnos.stream().anyMatch(alumno -> alumno.getEdad() >= 18));
+        System.out.println("------------------------------------------");
+
+        // Dame la edad de todos los alumnos que se llamen "Pepe" listado
+        // La operación termina con un List Integer que contiene las edades de los alumnos que se llaman "Pepe"
+        System.out.println("Edad de todos los alumnos que se llamen Pepe:");
+        //alumnos.stream().filter(alumno -> alumno.getNombre().equals("Pepe")).map(Alumno::getEdad).collect(Collectors.toList()).forEach(System.out::println);
+        List<Integer> edadesDePepe = alumnos.stream().filter(alumno -> alumno.getNombre().equals("Pepe")).map(Alumno::getEdad).collect(Collectors.toList());
+        System.out.println(edadesDePepe);
+        System.out.println("------------------------------------------");
+    
+        // método stream
+        //Ejemplo de uso o de algo: List<String> lista = Arrays.asList(new Alumno());
+        //lista.stream().map(Alumno::getEdad()).forEach(System.out::println);
+        //https://oregoom.com/java/comparator/
     }
 }
--- genericos/src/com/jve/modelo/Alumno.java
+++ genericos/src/com/jve/modelo/Alumno.java
@@ -1,13 +1,18 @@
 package com.jve.modelo;
 
+import java.util.Comparator;
+import java.util.Optional;
+
 public class Alumno implements Comparable<Alumno> {
 
 	private String nombre;
-    private String apellido;
+    private Optional<String> apellido;
+    private int edad;
 
-    public Alumno(String nombre, String apellido) {
-        this.nombre = nombre;
-        this.apellido = apellido;
+    public Alumno(String nombre, String apellido, int edad) {
+        this.nombre     = nombre;
+        this.apellido   = Optional.ofNullable(apellido);
+        this.edad       = edad;
     }
 
     public String getNombre() {
@@ -18,31 +23,66 @@ public class Alumno implements Comparable<Alumno> {
         this.nombre = nombre;
     }
 
-    public String getApellido() {
+    public Optional<String> getApellido() {
         return apellido;
     }
 
     public void setApellido(String apellido) {
-        this.apellido = apellido;
+        this.apellido = Optional.ofNullable(apellido);
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
     }
 
     @Override
     public String toString() {
         return "Alumno{" +
-               "nombre='" + nombre + '\'' +
-               ", apellido=" + apellido +
-               '}';
+            "nombre='" + nombre + '\'' +
+            ", apellido=" + apellido.orElse("No proporcionado") + 
+            ", edad='" + edad + '\'' +
+            '}';
     }
 
-	@Override
-	public int compareTo(Alumno o) {
-		
-		int nombreComparar = this.nombre.compareTo(o.nombre);
-		if (nombreComparar != 0) {
-			return nombreComparar;
-		}
-		
-		return this.apellido.compareTo(o.apellido);
-	}
+    // Natural order
+    @Override
+    public int compareTo(Alumno o) {
+
+        int nombreComparar = this.nombre.compareTo(o.nombre);
+        if (nombreComparar != 0) {
+            return nombreComparar;
+        }
+
+        int apellidoComparar = this.apellido.orElse("").compareTo(o.apellido.orElse(""));
+    
+        if (apellidoComparar != 0) {
+            return apellidoComparar;
+        }
+
+        return Integer.compare(this.edad, o.edad);
+    }
+
+    // Método estático para comparar por edad
+    public static Comparator<Alumno> compararPorEdad = new Comparator<Alumno>() {
+        @Override
+        public int compare(Alumno a1, Alumno a2) {
+            return Integer.compare(a1.getEdad(), a2.getEdad());
+        }
+    };
+
+    @Override
+    public boolean equals(Object obj) {
+
+        Alumno alumno = (Alumno) obj;
+        Integer edad = alumno.edad;
+
+        return nombre.equals(alumno.nombre) &&
+            apellido.equals(alumno.apellido) &&
+            edad.equals(alumno.edad);
+    }
 
 }
